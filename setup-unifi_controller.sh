d0346b02bbd4f51931613f0c41cb00b3cdb9e743
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-02-06 17:29:05 -0500

    base script

---
+++ setup-unifi_controller.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Check for root (SUDO).
+if [ "$EUID" -ne 0 ]
+then
+  clear
+  echo -e "${RED}#######################################${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#   ${RESET}Please run this script as root.   ${RED}#${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#######################################${RESET}"
+  exit 1
+fi
+
+echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg 
+apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50 
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get upgrade
+apt-get install oracle-java8-jdk unifi apt-transport-https
