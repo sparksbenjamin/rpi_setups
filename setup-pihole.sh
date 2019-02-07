bcb28ffb0ad20d1a03a3f25a662160f6d6b5e4cc
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-02-07 10:55:42 -0500

    Update setup-pihole.sh

--- setup-pihole.sh
+++ setup-pihole.sh
@@ -10,6 +10,16 @@ then
   echo -e "${RED}#######################################${RESET}"
   exit 1
 fi
+if pgrep -x "gedit" > /dev/null
+then
+  clear
+  echo -e "${RED}#######################################${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#   ${RESET}Known issues with bind9(named) service.   ${RED}#${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#######################################${RESET}"
+  exit 1
+fi
 curl -sSL https://install.pi-hole.net | bash
 wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/pi-hole-configs/pi-hole-update.sh" -O /etc/cron.daily/pi-hole-update.sh
 chmod +x /etc/cron.daily/pi-hole-update.sh
