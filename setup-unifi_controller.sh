6b62f2c40c18ebb934ce9bd06a0c7729f4ed0811
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-02-14 15:55:23 -0500

    Update setup-unifi_controller.sh

--- setup-unifi_controller.sh
+++ setup-unifi_controller.sh
@@ -14,7 +14,13 @@ fi
 echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg 
 apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50 
+apt-key adv --recv-key --keyserver keyserver.ubuntu.com EEA14886
 add-apt-repository ppa:webupd8team/java
+echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
+echo 'deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
 apt-get update
 apt-get -y upgrade
+#apt-get -y install oracle-java8-jdk
+apt-get -y install oracle-java8-installer
+apt-get -y install oracle-java8-set-default
 apt-get -y install oracle-java8-jdk unifi apt-transport-https
