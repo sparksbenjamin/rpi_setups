b3a5c6c560473ab07e13034d29438d32ac639a42
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-08-17 12:47:44 -0400

    Update setup-unifi_controller.sh

--- setup-unifi_controller.sh
+++ setup-unifi_controller.sh
@@ -11,19 +11,19 @@ then
   exit 1
 fi
 #setup unifi Install
+apt update --allow-releaseinfo-change
 apt update && apt -y install ca-certificates apt-transport-https haveged
 #apt-get install haveged
 echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt update
 wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg 
 apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
-
 #Setup MongoDB Install
 apt-key adv --keyserver keyserver.ubuntu.com --recv 9ECBEC467F0CEB10
 echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-apt-get update
+apt clean
+apt update
 
 #Begin Install
 apt install -y mongodb unifi openjdk-7-jdk
