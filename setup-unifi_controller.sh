a129144f41d812a2e99daef3b4832d2b5eb40dfa
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-08-17 12:42:44 -0400

    Update setup-unifi_controller.sh

--- setup-unifi_controller.sh
+++ setup-unifi_controller.sh
@@ -11,7 +11,8 @@ then
   exit 1
 fi
 #setup unifi Install
-apt update && apt install ca-certificates apt-transport-https
+apt update && apt -y install ca-certificates apt-transport-https haveged
+#apt-get install haveged
 echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
@@ -25,7 +26,7 @@ echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | tee 
 apt-get update
 
 #Begin Install
-apt install -y mongodb-org unifi
+apt install -y mongodb unifi openjdk-7-jdk
 
 #Setup Java Install
 #apt-key adv --recv-key --keyserver keyserver.ubuntu.com EEA14886
@@ -38,4 +39,4 @@ apt install -y mongodb-org unifi
 #apt-get -y install oracle-java8-installer
 #apt-get -y --allow-unauthenticated install oracle-java8-installer
 #apt-get -y install oracle-java8-set-default
-#apt-get -y install oracle-java8-jdk unifi apt-transport-https
+#apt-get -y install oracle-java8-jdk unifi
