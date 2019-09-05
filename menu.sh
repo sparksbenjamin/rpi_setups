f93c83508f6b653be51fe9491fea97a0de5880ae
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-09-05 14:11:59 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -6,19 +6,19 @@ options=("Unifi Controller" "Unifi VoIP Controller" "PI-Hole" "RetroPI" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Option 1")
+        "Unifi Controller")
             #echo "you chose choice 1"
 	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-unifi_controller.sh" -O setup-unifi_controller.sh && chmod +x setup-unifi_controller.sh && sudo ./setup-unifi_controller.sh && rm setup-unifi_controller.sh
             ;;
-        "Option 2")
+        "Unifi VoIP Controller")
             #echo "you chose choice 2"
 	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-unifi-voip-controller.sh" -O setup-unifi-voip-controller.sh && chmod +x setup-unifi-voip-controller.sh && sudo ./setup-unifi-voip-controller.sh && rm setup-unifi-voip-controller.sh
             ;;
-        "Option 3")
+        "PI-Hole")
             #echo "you chose choice $REPLY which is $opt"
 	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-pihole.sh" -O setup-pihole.sh && chmod +x setup-pihole.sh && sudo ./setup-pihole.sh && rm setup-pihole.sh
             ;;
-	"Option 4")
+	"RetroPI")
 	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-retropi.sh" -O setup-retropi.sh && chmod +x setup-retropi.sh && sudo ./setup-retropi.sh && rm setup-retropi.sh
 	    ;;
         "Quit")
