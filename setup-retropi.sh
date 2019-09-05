82a10f485170f72d71f5ca9d4bf1f8d985d35e28
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-09-05 08:15:13 -0400

    Update setup-retropi.sh

--- setup-retropi.sh
+++ setup-retropi.sh
@@ -1,3 +1,28 @@
+#! /bin/bash
+# Check for root (SUDO).
+if [ "$EUID" -ne 0 ]
+then
+  clear
+  echo -e "${RED}#######################################${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#   ${RESET}Please run this script as root.   ${RED}#${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#######################################${RESET}"
+  exit 1
+fi
+if pgrep -x "gedit" > /dev/null
+then
+  clear
+  echo -e "${RED}#######################################${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#   ${RESET}Known issues with bind9(named) service.   ${RED}#${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#######################################${RESET}"
+  exit 1
+fi
+apt update
+apt upgrade
+apt -y install git
 git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
 cd RetroPie-Setup
 sudo ./retropie_setup.sh
