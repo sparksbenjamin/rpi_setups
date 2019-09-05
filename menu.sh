2ea1153548910fa1d712f8491aa74fbc0344a36f
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-09-05 08:17:44 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,19 +2,25 @@
 # Bash Menu Script Example
 
 PS3='Please enter your choice: '
-options=("Unifi Controller" "Option 2" "Option 3" "Quit")
+options=("Unifi Controller" "Unifi VoIP Controller" "PI-Hole" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Option 1")
-            echo "you chose choice 1"
+            #echo "you chose choice 1"
+	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-unifi_controller.sh" -O setup-unifi_controller.sh && chmod +x setup-unifi_controller.sh && sudo ./setup-unifi_controller.sh && rm setup-unifi_controller.sh
             ;;
         "Option 2")
-            echo "you chose choice 2"
+            #echo "you chose choice 2"
+	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-unifi-voip-controller.sh" -O setup-unifi-voip-controller.sh && chmod +x setup-unifi-voip-controller.sh && sudo ./setup-unifi-voip-controller.sh && rm setup-unifi-voip-controller.sh
             ;;
         "Option 3")
-            echo "you chose choice $REPLY which is $opt"
+            #echo "you chose choice $REPLY which is $opt"
+	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-pihole.sh" -O setup-pihole.sh && chmod +x setup-pihole.sh && sudo ./setup-pihole.sh && rm setup-pihole.sh
             ;;
+	"Option 4")
+	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-retropi.sh" -O setup-retropi.sh && chmod +x setup-retropi.sh && sudo ./setup-retropi.sh && rm setup-retropi.sh
+	    ;;
         "Quit")
             break
             ;;
