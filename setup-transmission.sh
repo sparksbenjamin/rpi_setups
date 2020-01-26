30222a04359ad4d97478849a9f36f1bda88f7fab
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-01-26 13:53:55 -0500

    Update setup-transmission.sh

--- setup-transmission.sh
+++ setup-transmission.sh
@@ -1,1 +1,24 @@
-
+#! /bin/bash
+# Check for root (SUDO).
+if [ "$EUID" -ne 0 ]
+then
+  clear
+  echo -e "${RED}#######################################${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#   ${RESET}Please run this script as root.   ${RED}#${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#######################################${RESET}"
+  exit 1
+fi
+if pgrep -x "gedit" > /dev/null
+then
+  clear
+  echo -e "${RED}#######################################${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#   ${RESET}Known issues with bind9(named) service.   ${RED}#${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#######################################${RESET}"
+  exit 1
+fi
+apt update
+apt upgrade
