94590b0559cc59a9ac514fccbfbbe328b2c3a0af
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-03-02 16:10:10 -0500

    Update kiosk.sh

--- web-kiosk-config/kiosk.sh
+++ web-kiosk-config/kiosk.sh
@@ -3,6 +3,9 @@ xset s noblank
 xset s off
 xset -dpms
 
+rm -rf /home/pi/.cache;
+rm -rf /home/pi/.config;
+rm -rf /home/pi/.pki;
 unclutter -idle 0.5 -root &
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
