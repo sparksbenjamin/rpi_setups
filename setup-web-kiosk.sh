5729200af7675de2d376c8ae0848909f4743900c
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-03-02 15:30:05 -0500

    Update setup-web-kiosk.sh

--- setup-web-kiosk.sh
+++ setup-web-kiosk.sh
@@ -1,1 +1,26 @@
-
+#! /bin/bash
+# Check for root (SUDO).
+if [ "$EUID" -ne 0 ]
+then
+  clear
+  echo -e "${RED}#######################################${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#   ${RESET}Please run this script as root.   ${RED}#${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#######################################${RESET}"
+  exit 1
+fi
+if pgrep -x "gedit" > /dev/null
+then
+  clear
+  echo -e "${RED}#######################################${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#   ${RESET}Known issues with bind9(named) service.   ${RED}#${RESET}"
+  echo -e "${RED}#                                     #${RESET}"
+  echo -e "${RED}#######################################${RESET}"
+  exit 1
+fi
+curl -sSL https://install.pi-hole.net | bash
+wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/pi-hole-configs/pi-hole-update.sh" -O /etc/cron.daily/pi-hole-update.sh
+chmod +x /etc/cron.daily/pi-hole-update.sh
+exit
