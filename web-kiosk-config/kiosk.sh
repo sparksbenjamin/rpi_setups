28b68f9b935be1c1db17f16b2798eb28d2571b1a
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-03-03 08:21:18 -0500

    Update kiosk.sh
    Added Option for On-Screen Keyboard

--- web-kiosk-config/kiosk.sh
+++ web-kiosk-config/kiosk.sh
@@ -12,3 +12,6 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromi
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://time.is/
+
+#Uncomment to enable On-Screen Keyboard
+#matchbox-keyboard
