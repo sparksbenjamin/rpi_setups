42a83b8be5ee2cf3ed6a26c648fab222828fce96
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-10-16 19:31:25 -0400

    Update setup-home-assistant.sh

--- setup-home-assistant.sh
+++ setup-home-assistant.sh
@@ -18,8 +18,9 @@ sudo apt-get clean
 sudo apt-get autoremove -y
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install xdotool unclutter sed
-sudo apt-get install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev libtiff-dev autoconf build-essential libopenjp2-7 libjpeg-dev zlib1g-dev
+sudo apt-get install xdotool unclutter sed -y
+sudo apt-get install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev libtiff-dev autoconf build-essential libopenjp2-7 libjpeg-dev zlib1g-dev -y
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev tar wget vim -y
 sudo useradd -rm homeassistant -G dialout,gpio,i2c
 cd /srv
 sudo mkdir homeassistant
@@ -27,7 +28,8 @@ sudo chown homeassistant:homeassistant homeassistant
 sudo -u homeassistant -H -s
 cd /srv/homeassistant
 python3.8 -m venv .
-source bin/activate
+source  /srv/homeassistant/bin/activate
+PATH=$PATH:/home/homeassistant/.local/bin
 python3 -m pip install wheel
 pip3 install homeassistant
 exit
