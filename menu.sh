fd883805bd3aa44be50fceda0e6214eedb6e77a4
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-10-16 00:01:33 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -28,7 +28,7 @@ do
 	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-web-kiosk.sh" -O setup-web-kiosk.sh && chmod +x setup-web-kiosk.sh && sudo ./setup-web-kiosk.sh && rm setup-web-kiosk.sh
 	    ;;
 	"Home-Assistant"
-	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-web-kiosk.sh" -O setup-web-kiosk.sh && chmod +x setup-web-kiosk.sh && sudo ./setup-web-kiosk.sh && rm setup-web-kiosk.sh
+	    wget "https://raw.githubusercontent.com/sparksbenjamin/rpi_setups/master/setup-home-assistant.sh" -O setup-web-kiosk.sh && chmod +x setup-web-kiosk.sh && sudo ./setup-web-kiosk.sh && rm setup-web-kiosk.sh
 	    ;;
         "Quit")
             break
